37de40b65511b2a111522ec9c8b630263367efb5
Artur Gomes <flashballclube@gmail.com>
2018-04-06 11:20:54 -0300

    [Fix] Rebuild Virtual env
    - Fix default python in virtual env

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/arturgomes/Documents/Teste-Tecnico"
+set -gx VIRTUAL_ENV "/Users/arturgomes/Documents/Teste-Tecnico/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
