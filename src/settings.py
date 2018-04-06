4b1125a7933edceb29e515dc0a5f9a8fafd14fde
Artur Gomes <flashballclube@gmail.com>
2018-04-06 09:26:13 -0300

    [Feature] Add Static files support
    -Add static files support
    -Add static path in settings file

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = BASE_DIR +'/src/static/'
+STATIC_URL = 'src/static/'
 PORT=3000
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,13 +1,28 @@
 import cgi
+import src.settings as conf
 from src.controllers.index import *
 from src.controllers.error_404 import *
 
-def app(environ, start_response):
+def router(environ, start_response):
 
     if environ['PATH_INFO'] == '/':
         html = index(environ)
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return [html]
+
+    elif environ['PATH_INFO'] == '/css/style.css':
+        f = open( conf.STATIC_URL + 'css/style.css')
+        html = str.encode(f.read())
+        start_response('200 OK', [('Content-Type', 'text/css')])
+        return [html]
+
+    elif environ['PATH_INFO'] == '/js/main.js':
+        f = open( conf.STATIC_URL + 'js/main.js')
+        html = str.encode(f.read())
+        start_response('200 OK', [('Content-Type', 'text/js')])
+        return [html]        
+
     else:
         html = error_404(environ)
-     
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [html]
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return [html]
