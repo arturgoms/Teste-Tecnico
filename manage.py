d28abb446fd2489cbd02d5fe961cc427ecf837a0
Artur Gomes <flashballclube@gmail.com>
2018-04-06 09:27:07 -0300

    [Fix] Better name for router function

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from src.wsgi import app
+from src.wsgi import router
 import src.settings as conf
 class server():
     def __init__(self):
         try:
             from wsgiref.simple_server import make_server
-            httpd = make_server('', conf.PORT, app)
+            httpd = make_server('', conf.PORT, router)
             print('Serving on port {}'.format(conf.PORT))
             httpd.serve_forever()
         except KeyboardInterrupt:
