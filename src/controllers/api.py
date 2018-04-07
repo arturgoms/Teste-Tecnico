4771c4e5a5591928d76686d82b399a60cd7f855a
Artur Gomes <flashballclube@gmail.com>
2018-04-07 20:43:12 -0300

    [Feature] PUT DELETE POST Method
    -Remove all the duplicate things
    -Add DELETE method
    -Add PUT method
    -Add POST method
    -All this is working in the same page withour refresh

--- src/controllers/api.py
+++ src/controllers/api.py
@@ -12,18 +12,47 @@ import cgi
 import json
 import src.models.mysql as mysql
 
+def getFields(environ):
+        """
+        getFields function:
+            retorna os campos que chegam pelos metodos POST DELETE PUT
+        """
+        data_env = environ.copy()
+        data_env['QUERY_STRING'] = ''
+        data = cgi.FieldStorage(
+            fp=environ['wsgi.input'],
+            environ=data_env,
+            keep_blank_values=True
+        )
+        return data
+
 def api(environ, start_response):
     """
         api function:
             Lógica para a rota api
     """
     
+    # Metodo POST, adiciona um contato no banco
     if environ['REQUEST_METHOD'] == 'POST':
-        html = str.encode(json.dumps({"REQUEST_METHOD": "POST"}))
-    else:
+        post = getFields(environ)
         db = mysql.MySQL()
-        json_data = db.select('users')
-        html = str.encode(json.dumps(json_data))
+        db.insert('users', {"nome":post['nome'].value, "sobrenome":post['sobrenome'].value, "endereco":post['endereco'].value })
 
+    # Metodo DELETE, deleta um contato do banco
+    if environ['REQUEST_METHOD'] == 'DELETE':
+        delete = getFields(environ)
+        db = mysql.MySQL()
+        db.delete_where('users', 'id = {}'.format(delete['id'].value))
+
+    # Metodo PUT, atualiza um contato 
+    if environ['REQUEST_METHOD'] == 'PUT':
+        put = getFields(environ)
+        db = mysql.MySQL()
+        db.update_where('users',"nome = '"+ put['nome'].value +"', sobrenome = '"+ put['sobrenome'].value +"', endereco = '"+ put['endereco'].value + "'", 'id = ' + put['id'].value)
+
+    # Método GET, retorna todos os contatos no banco
+    db = mysql.MySQL()
+    json_data = db.select('users')
+    html = str.encode(json.dumps(json_data))
     start_response('200 OK', [('Content-Type', 'text/json')])
     return [html]
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,31 +1,109 @@
 $(document).ready(function () {
+    updateTable();
+    setInterval(updateTable, 1000);
 
-    // add a variable that remains through closure
-    var lastTimeResponse = [];
+});
 
-    function getRealData() {
-        $.get("api", function(dados, status){
-            response = $.parseJSON(dados);
-            //console.log(response)
-            //var vehicleListData = '';
+function updateTable() {
+    $.ajax({
+        type: 'GET',
+        url: 'api',
+        success: function (response) {
+            dados = $.parseJSON(response);
             var userList = '';
-            $.each(response, function(key, value) {
-                userList += '<tr id="rowVehicleStatus" class="">';
-                userList += '<td>'+key+'</td>';
+            $.each(dados, function(key, value) {
+                userList += '<tr id="rowUsers" class="">';
                 userList += '<td>'+value["nome"]+'</td>';
                 userList += '<td>'+value["sobrenome"]+'</td>';
                 userList += '<td>'+value["endereco"]+'</td>';
-                userList += '</tr>';     
+                userList += '<td><a id="showUpdateField" onclick="showUpdateField('+ key + ');" class="submit btn">Editar</a>&nbsp<a onclick="deleteField('+ key +');" class="submit btn">Deletar</a></td>';
+                userList += '</tr>';    
             });
             $('#tabela').html(userList);
+        },
+        error: function(result) {
 
-        });
-    }
+        }
+    });
+}
+function addField() {
+    var nome = $("#addNome").val();
+    var sobrenome = $("#addSobrenome").val();
+    var endereco = $("#addEndereco").val();
+    $.ajax({
+        type: 'POST',
+        url: 'api',
+        data: {
+            nome:nome,
+            sobrenome:sobrenome,
+            endereco:endereco
+        },
+        success: function (response) {
+            
+        },
+        error: function(result) {
+            // Do something with the result
+        }
+    });
+    return false;
+}
 
-    //this refreshes data every 2seconds
-    setInterval(getRealData, 1000);
+function deleteField(id){
+    $.ajax({
+        url: 'api',
+        type: 'DELETE',
+        data: {
+            id:id
+        },
+        success: function(result) {
+            // Do something with the result
+        },
+        error: function(result) {
+            // Do something with the result
+        }
+    });
+}
 
-    //call the function to display data
-    getRealData();
+function showUpdateField(id){
+    $.ajax({
+        type: 'GET',
+        url: 'api',
+        success: function (response) {
+            dados = $.parseJSON(response);
+            $('#updateId').val(id);
+            $('#updateNome').val(dados[id]['nome']);
+            $('#updateSobrenome').val(dados[id]['sobrenome']);
+            $('#updateEndereco').val(dados[id]['endereco']);
+            $('#updateField').show("fade", { direction: "right" }, 100);
+        },
+        error: function(result) {
 
-});
+        }
+    });
+    
+}
+
+function updateField(){
+    var id = $("#updateId").val();
+    var nome = $("#updateNome").val();
+    var sobrenome = $("#updateSobrenome").val();
+    var endereco = $("#updateEndereco").val();
+    $.ajax({
+        url: 'api',
+        type: 'PUT',
+        data: {
+            id:id,
+            nome:nome,
+            sobrenome:sobrenome,
+            endereco:endereco
+        },
+        success: function(response) {
+         //...
+        },
+        error: function(result) {
+            // Do something with the result
+        }
+    });
+    $('#updateField').hide("fade", { direction: "right" }, 100);
+return false;
+}
