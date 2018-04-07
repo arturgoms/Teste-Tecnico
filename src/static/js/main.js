bf44df480a97e02fdc4a5445a1f8d0d5b29ffda1
Artur Gomes <flashballclube@gmail.com>
2018-04-06 21:26:25 -0300

    [Feature] Update Index without Reload
    -Add Ajax function to mirror db

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,20 +6,26 @@ $(document).ready(function () {
     function getRealData() {
         $.get("api", function(dados, status){
             response = $.parseJSON(dados);
-
-            $(function() {
-            $.each(response, function(i, item) {
-                var $tr = $('<tr>').append(
-                    $('<td>').text(i),
-                    $('<td>').text(item["nome"]),
-                    $('<td>').text(item["sobrenome"]),
-                    $('<td>').text(item["endereco"])
-                ); //.appendTo('#records_table');
-            $("#tabela").append($tr)
+            //console.log(response)
+            //var vehicleListData = '';
+            var userList = '';
+            $.each(response, function(key, value) {
+                userList += '<tr id="rowVehicleStatus" class="">';
+                userList += '<td>'+key+'</td>';
+                userList += '<td>'+value["nome"]+'</td>';
+                userList += '<td>'+value["sobrenome"]+'</td>';
+                userList += '<td>'+value["endereco"]+'</td>';
+                userList += '</tr>';     
             });
+            $('#tabela').html(userList);
+
         });
-    });
     }
+
+    //this refreshes data every 2seconds
+    setInterval(getRealData, 1000);
+
+    //call the function to display data
     getRealData();
 
 });
