06f716ff14e2d47dd3bd95f8b9f80f671dd319cf
Artur Gomes <flashballclube@gmail.com>
2018-04-07 20:41:17 -0300

    [Fix] Remove unessecery things

--- src/controllers/index.py
+++ src/controllers/index.py
@@ -17,16 +17,5 @@ def index(environ, start_response):
     """
     template = open('src/templates/index.html')
     html = str.encode(template.read())
-    
-    if environ['REQUEST_METHOD'] == 'POST':
-        post_env = environ.copy()
-        post_env['QUERY_STRING'] = ''
-        post = cgi.FieldStorage(
-            fp=environ['wsgi.input'],
-            environ=post_env,
-            keep_blank_values=True
-        )
-        html = str.encode("Hello, %s!"%post['name'].value)
-
     start_response('200 OK', [('Content-Type', 'text/html')])
     return [html]
